89046533d0d8767d6ccccdb6f4a8236f70443b73
bolilloro61 <roymosque@gmail.com>
2025-10-23 11:48:31 -0600

    [ADDED] Se agrego un demo para un select y el enlace de las APIs de paises y viaje

--- js/script.js
+++ js/script.js
@@ -1,10 +1,49 @@
-const apiKey = 'a846fd3683392957148d860e9e26500e'; // Reemplaza con tu clave real
-const lat = 33.44;
-const lon = -94.04;
+const APIcountries_name = "https://restcountries.com/v3.1/all?fields=name";
+const countriesSelect = document.getElementById("countries");
 
-const url = "http://api.openweathermap.org/data/2.5/find?q=Palo+Alto&units=metric&type=accurate&mode=JSON&APPID=a846fd3683392957148d860e9e26500e"
+async function fetchCountries() {
+  const response = await fetch(APIcountries_name);
+  const data = await response.json();
 
-fetch(url)
+  data.sort((a, b) => a.name.common.localeCompare(b.name.common));
+
+  countriesSelect.innerHTML = "";
+  data.forEach(country => {
+    const option = document.createElement("option");
+    option.value = country.name.common;
+    option.textContent = country.name.common;
+    countriesSelect.appendChild(option);
+  });
+}
+
+fetchCountries();
+
+// Escuchar cuando el usuario seleccione un país
+countriesSelect.addEventListener('change', () => {
+  const textoSeleccionado = countriesSelect.options[countriesSelect.selectedIndex].text;
+  
+  const APIbooking = new XMLHttpRequest();
+  APIbooking.withCredentials = true;
+
+  APIbooking.addEventListener('readystatechange', function () {
+    if (this.readyState === this.DONE) {
+      console.log(this.responseText);
+    }
+  });
+
+  const url = `https://booking-com15.p.rapidapi.com/api/v1/attraction/searchLocation?query=${encodeURIComponent(textoSeleccionado)}&languagecode=en-us`;
+  
+  APIbooking.open('GET', url);
+  APIbooking.setRequestHeader('x-rapidapi-key', '913dc042bamsh64559777b9efd1ap1f0cebjsnb5235ae74968');
+  APIbooking.setRequestHeader('x-rapidapi-host', 'booking-com15.p.rapidapi.com');
+
+  APIbooking.send(null);
+});
+
+
+const APIweather = "http://api.openweathermap.org/data/2.5/find?q=Palo+Alto&units=metric&type=accurate&mode=JSON&APPID=a846fd3683392957148d860e9e26500e"
+
+fetch(APIweather)
   .then(response => {
     if (!response.ok) {
       throw new Error('Error en la solicitud: ' + response.status);
@@ -22,35 +61,31 @@ fetch(url)
 
 const dat = null;
 
-const xhr = new XMLHttpRequest();
-xhr.withCredentials = true;
 
-xhr.addEventListener('readystatechange', function () {
-	if (this.readyState === this.DONE) {
-		console.log(this.responseText);
-	}
-});
 
-xhr.open('GET', 'https://booking-com15.p.rapidapi.com/api/v1/cars/searchCarRentals?pick_up_latitude=40.6397018432617&pick_up_longitude=-73.7791976928711&drop_off_latitude=40.6397018432617&drop_off_longitude=-73.7791976928711&pick_up_time=10%3A00&drop_off_time=10%3A00&driver_age=30&currency_code=USD&location=US');
-xhr.setRequestHeader('x-rapidapi-key', '913dc042bamsh64559777b9efd1ap1f0cebjsnb5235ae74968');
-xhr.setRequestHeader('x-rapidapi-host', 'booking-com15.p.rapidapi.com');
 
-xhr.send(dat);
+
 const da = null;
 
-const xh = new XMLHttpRequest();
-xh.withCredentials = true;
+const APIastro = new XMLHttpRequest();
+APIastro.withCredentials = true;
 
-xh.addEventListener('readystatechange', function () {
+APIastro.addEventListener('readystatechange', function () {
 	if (this.readyState === this.DONE) {
 		console.log(this.responseText);
 	}
 });
 
-xh.open('GET', 'https://astropredict-daily-horoscopes-lucky-insights.p.rapidapi.com/horoscope?lang=es&zodiac=aquarius&type=daily&timezone=UTC');
-xh.setRequestHeader('x-rapidapi-key', '913dc042bamsh64559777b9efd1ap1f0cebjsnb5235ae74968');
-xh.setRequestHeader('x-rapidapi-host', 'astropredict-daily-horoscopes-lucky-insights.p.rapidapi.com');
+APIastro.open('GET', 'https://astropredict-daily-horoscopes-lucky-insights.p.rapidapi.com/horoscope?lang=es&zodiac=aquarius&type=daily&timezone=UTC');
+APIastro.setRequestHeader('x-rapidapi-key', '913dc042bamsh64559777b9efd1ap1f0cebjsnb5235ae74968');
+APIastro.setRequestHeader('x-rapidapi-host', 'astropredict-daily-horoscopes-lucky-insights.p.rapidapi.com');
 
-xh.send(da);
+APIastro.send(da);
 
-countries="https://restcountries.com/v3.1/all"
+
+
+
+
+
+
+
